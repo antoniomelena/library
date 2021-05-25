ca2ba93fe148a658d73af3dd881f1edc135efb25
Antonio Melena <75593608+antoniomelena@users.noreply.github.com>
2021-05-24 19:34:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const myLibrary = [
+  ["The Song Of Achilles", "Madeline Miller", 416],
+  ["The Picture of Dorian Gray", "Oscar Wilde", 176],
+  ["The Girl with the Dragon Tattoo", "Stieg Larsson", 600],
+];
+
+function Book() {}
+
+function addBookToLibrary() {}
