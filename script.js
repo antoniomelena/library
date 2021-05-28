12f6e445f0f89873c274345864855394bf9efd9d
Antonio Melena <melenaantonio@gmail.com>
2021-05-28 16:45:03 -0500

    scratched the table and went for a card layout, created a createbookcard function

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-const authorInput = document.getElementById("author");
-const titleInput = document.getElementById("title");
-const pagesInput = document.getElementById("pages");
+// const authorInput = document.getElementById("author");
+// const titleInput = document.getElementById("title");
+// const pagesInput = document.getElementById("pages");
+const addBook = document.getElementById("addBookButton");
+const popUp = document.querySelector(".bg-modal");
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+function addBookToLibrary(newBook) {
+  if (myLibrary.some((book) => book.title === newBook.title)) {
+    return false;
+  }
+  myLibrary.push(newBook);
+  return true;
 }
 
 const bookOne = new Book("The Song Of Achilles", "Madeline Miller", 416, false);
@@ -22,33 +30,63 @@ const bookTwo = new Book(
   176,
   true
 );
-const bookFour = new Book("A Little Life", "Hanya Yanagihara", 800, false);
+const bookThree = new Book("A Little Life", "Hanya Yanagihara", 800, true);
 addBookToLibrary(bookOne);
 addBookToLibrary(bookTwo);
-addBookToLibrary(bookFour);
+addBookToLibrary(bookThree);
 
-function fillTable(data) {
-  let table = document.getElementById("libraryTable");
-
-  for (const book of data) {
-    const row = `<tr>
-                <td>${book.title}</td>
-                <td>${book.author}</td>
-                <td>${book.pages}</td>
-                <td>${book.read}</td>
-                </tr>`;
-    table.innerHTML += row;
-  }
+function openModal() {
+  // form.reset();
+  popUp.style.display = "flex";
 }
 
-fillTable(myLibrary);
+addBook.addEventListener("click", openModal);
 
-document.querySelector(".button").addEventListener("click", fillTable());
+// fillTable(myLibrary);
 
-document.getElementById("bookButton").addEventListener("click", () => {
-  document.querySelector(".bg-modal").style.display = "flex";
-});
+// addBook.addEventListener("click", () => {
+//   document.querySelector(".bg-modal").style.display = "flex";
+// });
 
 document.querySelector(".close").addEventListener("click", () => {
   document.querySelector(".bg-modal").style.display = "none";
 });
+
+const booksGrid = document.querySelector(".books-grid");
+
+function createBookCard(book) {
+  const bookCard = document.createElement("div");
+  const title = document.createElement("h2");
+  const author = document.createElement("h3");
+  const pages = document.createElement("h3");
+  const readButton = document.createElement("button");
+  const removeButton = document.createElement("button");
+
+  bookCard.classList.add("book-card");
+  title.classList.add("book-text");
+  author.classList.add("book-text");
+  pages.classList.add("book-text");
+  readButton.classList.add("button");
+  removeButton.classList.add("button");
+  removeButton.classList.add("button--red");
+
+  title.textContent = book.title;
+  author.textContent = `by ${book.author}`;
+  pages.textContent = `${book.pages} pages`;
+  removeButton.textContent = "Delete";
+  readButton.style.width = "1fr";
+  if (book.read) {
+    readButton.textContent = "Read";
+    readButton.classList.add("read-button");
+  } else {
+    readButton.textContent = "Not Read";
+    readButton.classList.add("not-read-button");
+  }
+
+  booksGrid.appendChild(bookCard);
+  bookCard.appendChild(title);
+  bookCard.appendChild(author);
+  bookCard.appendChild(pages);
+  bookCard.appendChild(readButton);
+  bookCard.appendChild(removeButton);
+}
